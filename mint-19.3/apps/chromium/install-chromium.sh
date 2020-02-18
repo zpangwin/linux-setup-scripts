89cc842618b92cf23b49e900513153bd03f8046f
zpangwin <zpangwin@users.noreply.github.com>
2020-02-18 17:02:56 -0500

    updated chromium script; the --password-store=basic option causes chrome/ium to store passwords in plaintext on the system which is not desirable for security

--- mint-19.3/apps/chromium/install-chromium.sh
+++ mint-19.3/apps/chromium/install-chromium.sh
@@ -2,10 +2,5 @@
 
 sudo apt update;
 
-sudo apt install -y --install-recommends chromium-browser;
+sudo apt install -y --install-recommends libpam-gnome-keyring chromium-browser;
 
-# fix shortcut to avoid stupid keyring prompts
-if [[ ! -e /usr/share/applications/chromium-browser.desktop.orig ]]; then
-    cp -a /usr/share/applications/chromium-browser.desktop /usr/share/applications/chromium-browser.desktop.orig;
-fi
-sudo sed -i -E 's/^(Exec=chromium\-browser)( \-\-incognito| %U|)$/\1 \-\-password-store=basic \2/g' /usr/share/applications/chromium-browser.desktop;
