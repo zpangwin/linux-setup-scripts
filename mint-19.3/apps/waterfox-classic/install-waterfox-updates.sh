3e50d3de61e4b1a6bc73d9ea9c26495a0d42aaa8
zpangwin <zpangwin@users.noreply.github.com>
2020-02-18 17:15:28 -0500

    Waterfox script - Fixes for various minor issues encountered on other machines to better handle icons, symlinks, and shortcuts after the main install

--- mint-19.3/apps/waterfox-classic/install-waterfox-updates.sh
+++ mint-19.3/apps/waterfox-classic/install-waterfox-updates.sh
@@ -43,11 +43,11 @@ ARG1="$1";
 ARG2="$2";
 WATERFOX_DOWNLOAD_PAGE="https://www.waterfox.net/releases/";
 CHROME_WINDOWS_UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36";
-WATERFOX_MENU_LINK="/usr/share/applications/waterfox.desktop";
-PRIVATE_MENU_LINK="/usr/share/applications/waterfox-private-browsing.desktop";
+WATERFOX_MENU_LINK="/usr/share/applications/waterfox-classic.desktop";
+PRIVATE_MENU_LINK="/usr/share/applications/waterfox-classic-private.desktop";
 PRIVATE_ICON_NAME="private-browsing";
 PRIVATE_ICON_PATH="/usr/share/icons/${PRIVATE_ICON_NAME}.png";
-WATERFOX_ICON_NAME="waterfox";
+WATERFOX_ICON_NAME="waterfox-classic";
 WATERFOX_ICON_PATH="/usr/share/icons/${WATERFOX_ICON_NAME}.png";
 
 if [[ "-h" == "${ARG1}" || "--help" == "${ARG1}" ]]; then
@@ -245,7 +245,7 @@ fi
 #Create a dir for storing installed archives, if it doesn't already exist
 ARCHIVE_DIR="${PARENT_DIR_WHERE_WE_WILL_EXTRACT_TO}/waterfox-archives";
 if [[ ! -e "${ARCHIVE_DIR}" ]]; then
-	sudo mkdir -p "${ARCHIVE_DIR}";
+	sudo mkdir -p "${ARCHIVE_DIR}" 2>/dev/null;
 fi
 echo ''
 echo "Backup archives will be stored in ARCHIVE_DIR: ${ARCHIVE_DIR}";
@@ -331,11 +331,16 @@ else
 fi
 
 # On successful install...
+echo "Checking for successful install...";
+echo "   WATERFOX_INSTALL_DIR: '${WATERFOX_INSTALL_DIR}'";
+echo "   WATERFOX_ICON_PATH:   '${WATERFOX_ICON_PATH}'";
+echo "   PRIVATE_ICON_PATH:    '${PRIVATE_ICON_PATH}'";
+echo "   WATERFOX_MENU_LINK:   '${WATERFOX_MENU_LINK}'";
+echo "   PRIVATE_MENU_LINK:    '${PRIVATE_MENU_LINK}'";
+
 if [[ -e "${WATERFOX_INSTALL_DIR}/waterfox" ]]; then
-	# Make sure system link exists
-	if [[ ! -e /usr/bin/waterfox ]]; then
-		sudo ln -s "${WATERFOX_INSTALL_DIR}/waterfox" /usr/bin/waterfox;
-	fi
+	# Make sure system link exists / that its pointing to the correct locaton
+	sudo ln -sf "${WATERFOX_INSTALL_DIR}/waterfox" /usr/bin/waterfox;
 
     # Make sure regular icon exists
     if [[ ! -e "${WATERFOX_ICON_PATH}" ]]; then
@@ -348,22 +353,69 @@ if [[ -e "${WATERFOX_INSTALL_DIR}/waterfox" ]]; then
 
 	# Make sure private browsing icon exists
 	if [[ ! -e "${PRIVATE_ICON_PATH}" ]]; then
-		PBICON_LINK="https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/branding/official/pbmode.ico";
-		TMP_DIR="/tmp/private-browsing-icon";
-		PB_TMP_FILE="${TMP_DIR}/pbmode.ico";
-		mkdir "${TMP_DIR}";
-		cd "${TMP_DIR}";
-		/usr/bin/wget --user-agent "${CHROME_WINDOWS_UA}" "${PBICON_LINK}" --output-document="${PB_TMP_FILE}" 2>/dev/null;
-		if [[ ! -e /usr/bin/convert ]]; then
-            sudo apt update;
-			sudo apt install -y imagemagick;
+		# if the ../firefox/install-private-browsing-icon.sh install was successful, then just re-copy that image to save time/bandwidth
+		if [[ -e /usr/share/icons/private-browsing.png ]]; then
+			sudo cp -a /usr/share/icons/private-browsing.png "${PRIVATE_ICON_PATH}";
+		else
+			PBICON_LINK="https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/branding/official/pbmode.ico";
+			TMP_DIR="/tmp/private-browsing-icon";
+			PB_TMP_FILE="${TMP_DIR}/pbmode.ico";
+			mkdir "${TMP_DIR}" 2>/dev/null;
+			/usr/bin/wget --user-agent "${CHROME_WINDOWS_UA}" "${PBICON_LINK}" --output-document="${PB_TMP_FILE}" 2>/dev/null;
+			if [[ ! -e /usr/bin/convert ]]; then
+		        sudo apt update;
+				sudo apt install -y imagemagick;
+			fi
+			# ================================================================================================================
+			# Workaround
+			# ================================================================================================================
+			# As of ImageMagick 6.9.7-4, convert was VERY buggy for me. It had two issues:
+			#	* Would not work with absolute paths; would only work with working dir as parent of input file
+			#	* Would generate multiple files appended with numeric indexes instead of the single specified file.
+			#
+			# With regards to the issue with absolute paths for input/output files.
+			# something like the following:
+			#		cd $HOME && convert /tmp/private-browsing-icon/pbmode.ico /tmp/private-browsing-icon/pbmode.png
+			# would fail with the error:
+			# 		convert-im6.q16: missing an image filename `pbmode.png' @ error/convert.c/ConvertImageCommand/3255
+			#
+			# while the command
+			# 		cd /tmp/private-browsing-icon && convert pbmode.ico pbmode.png
+			# would "succeed" without any conversion issues but would create
+			#		$ ls -l
+			#		total 16
+			#		-rw-rw---- 1 zzz zzz  930 Feb  2 15:10 pbmode-0.png
+			#		-rw-rw---- 1 zzz zzz 1615 Feb  2 15:10 pbmode-1.png
+			#		-rw-rw---- 1 zzz zzz 6518 Feb  2 14:50 pbmode.ico
+			#
+			# instead of the specified "pbmode.png"
+			# ================================================================================================================
+			if [[ ! -e "${TMP_DIR}" ]]; then
+				echo "ERROR: Unable to create temp dir at '${TMP_DIR}'";
+			else
+				STARTING_DIR=$(pwd);
+				cd "${TMP_DIR}";
+				convert "${PB_TMP_FILE}" "${PB_TMP_FILE%.*}.png";
+				cd "${TMP_DIR}";
+
+				if [[ -e "${PB_TMP_FILE%.*}.png" ]]; then
+					sudo cp "${PB_TMP_FILE%.*}.png" "${PRIVATE_ICON_PATH}";
+					sudo chown root:root "${PRIVATE_ICON_PATH}";
+					sudo chmod a+r "${PRIVATE_ICON_PATH}";
+
+				elif [[ -e "${PB_TMP_FILE%.*}-0.png" ]]; then
+					sudo cp "${PB_TMP_FILE%.*}-0.png" "${PRIVATE_ICON_PATH}";
+					sudo chown root:root "${PRIVATE_ICON_PATH}";
+					sudo chmod a+r "${PRIVATE_ICON_PATH}";
+
+				elif [[ -e "${PB_TMP_FILE%.*}-1.png" ]]; then
+					sudo cp "${PB_TMP_FILE%.*}-1.png" "${PRIVATE_ICON_PATH}";
+					sudo chown root:root "${PRIVATE_ICON_PATH}";
+					sudo chmod a+r "${PRIVATE_ICON_PATH}";
+
+				fi
+			fi
 		fi
-		convert "${PB_TMP_FILE}" "${PB_TMP_FILE%.*}.png";
-        if [[ -e "${PB_TMP_FILE%.*}.png" ]]; then
-            sudo cp "${PB_TMP_FILE%.*}.png" "${PRIVATE_ICON_PATH}";
-    		sudo chown root:root "${PRIVATE_ICON_PATH}";
-    		sudo chmod a+r "${PRIVATE_ICON_PATH}";
-        fi
 	fi
 
 	# Make sure system menu exists
@@ -400,34 +452,54 @@ if [[ -e "${WATERFOX_INSTALL_DIR}/waterfox" ]]; then
 
 	# Make sure system menu for private browsing exists
 	if [[ ! -f "${PRIVATE_MENU_LINK}" ]]; then
-		echo '#!/usr/bin/env xdg-open'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo '[Desktop Entry]'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Version=1.0'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Name=Waterfox (Private Browsing)'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Comment=Firefox fork that supports Legacy Add-ons includes Mozilla security patches.'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Keywords=Internet;WWW;Browser;Web;Explorer'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Exec=waterfox -private-window https://www.dnsleaktest.com/'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Terminal=false'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'X-MultipleArgs=false'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Type=Application'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo "Icon=${PRIVATE_ICON_NAME}"|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Categories=GNOME;GTK;Network;WebBrowser;'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'StartupNotify=true'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Actions=new-window;new-private-window;'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Name[en_US]=Private (WF)'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo '[Desktop Action new-window]'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Name=Open a New Window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Exec=waterfox -new-window -private-window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo '[Desktop Action new-private-window]'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Name=Open a New Private Window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo 'Exec=waterfox -private-window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
-		echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+		if [[ -f "${WATERFOX_MENU_LINK}" ]]; then
+			# copy original
+			sudo cp -a "${WATERFOX_MENU_LINK}" "${PRIVATE_MENU_LINK}";
 
+			# remove internationalization (less false positives)
+			sudo sed -i -E '/^.*(Name|Keywords|Comment)\[\w+].*$/d' "${PRIVATE_MENU_LINK}";
+
+			# remove the regular new window action
+			sudo sed -i -E 's/^(Actions=.*)\bNewWindow;(.*)$/\1\2/g' "${PRIVATE_MENU_LINK}";
+			sudo sed -i -z -E 's/\n\[Desktop Action NewWindow\]\nName=[^\n]+\nExec=[^\n]+\n//g' "${PRIVATE_MENU_LINK}";
+
+			# change name
+			sudo sed -i -E 's/^(Name=Waterfox.*)/\1 (Private Browsing)/g' "${PRIVATE_MENU_LINK}";
+
+			# change icon
+			sudo sed -i -E "s/^(Icon=).*/\\1${PRIVATE_ICON_NAME}/g" "${PRIVATE_MENU_LINK}";
+
+			# change default exec arg
+			sudo sed -i -E 's|^(Exec=waterfox\S*) %u$|\1 -private-window https://www.dnsleaktest.com/|g' "${PRIVATE_MENU_LINK}";
+		else
+			echo '#!/usr/bin/env xdg-open'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo '[Desktop Entry]'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Version=1.0'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Name=Waterfox (Private Browsing)'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Comment=Firefox fork that supports Legacy Add-ons includes Mozilla security patches.'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Keywords=Internet;WWW;Browser;Web;Explorer'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Exec=waterfox -private-window https://www.dnsleaktest.com/'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Terminal=false'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'X-MultipleArgs=false'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Type=Application'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo "Icon=${PRIVATE_ICON_NAME}"|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Categories=GNOME;GTK;Network;WebBrowser;'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'StartupNotify=true'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Actions=new-window;new-private-window;'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Name[en_US]=Private (WF)'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo '[Desktop Action new-window]'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Name=Open a New Window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Exec=waterfox -new-window -private-window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo '[Desktop Action new-private-window]'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Name=Open a New Private Window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo 'Exec=waterfox -private-window'|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+			echo ''|sudo tee -a "${PRIVATE_MENU_LINK}" 2>&1 >/dev/null;
+		fi
 		sudo chown root:root "${PRIVATE_MENU_LINK}";
 		sudo chmod a+rx "${PRIVATE_MENU_LINK}";
 	fi
