88938e0a0c1ac7650b1424bbae128a044027e302
zpangwin <zpangwin@users.noreply.github.com>
2020-02-18 21:58:52 -0500

    git extensions script - changed apt to apt-get for better compat

--- mint-19.3/apps/mono+git-extensions/install-mono-and-git-extensions-2.5.sh
+++ mint-19.3/apps/mono+git-extensions/install-mono-and-git-extensions-2.5.sh
@@ -21,7 +21,7 @@ echo "Installing Depenencies from central repos...";
 echo "================================================================";
 
 sudo dpkg --add-architecture i386;
-sudo apt install --install-recommends -y curl git imagemagick language-pack-en perl unzip wget;
+sudo apt-get install --install-recommends -q -y curl git imagemagick language-pack-en perl unzip wget;
 
 if [[ ! -e /etc/apt/sources.list.d/mono-official-stable.list ]]; then
 	echo "";
@@ -37,12 +37,12 @@ if [[ ! -e /etc/apt/sources.list.d/mono-official-stable.list ]]; then
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 	echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list;
     sudo chmod 644 /etc/apt/sources.list.d/mono-official-stable.list;
-	sudo apt update --yes;
+	sudo apt-get update --quiet --yes;
 fi
 
 MONO_PACKAGES="mono-runtime mono-devel mono-complete referenceassemblies-pcl ca-certificates-mono mono-xsp4";
 echo -e "\nRunning: apt install --install-recommends -y $MONO_PACKAGES";
-sudo apt install --install-recommends -y $MONO_PACKAGES;
+sudo apt-get install --install-recommends -q -y $MONO_PACKAGES;
 
 echo "";
 echo "================================================================";
