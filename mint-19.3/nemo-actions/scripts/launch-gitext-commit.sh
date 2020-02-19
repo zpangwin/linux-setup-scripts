d7dfdc0b7acaed13d84cb6072e30c0ee4e2624e3
zpangwin <zpangwin@users.noreply.github.com>
2020-02-18 22:04:20 -0500

    recopied /usr scripts + chmod a+rx *.sh

--- mint-19.3/nemo-actions/scripts/launch-gitext-commit.sh
+++ mint-19.3/nemo-actions/scripts/launch-gitext-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gitTopLevelDir=$(/usr/bin/which-git-top-dir "$1");
+gitTopLevelDir=$("/usr/bin/which-git-top-dir" "$1");
 if [[ "" != "${gitTopLevelDir}" ]]; then
 	cd "${gitTopLevelDir}";
     /usr/bin/mono "/opt/GitExtensions/GitExtensions.exe" commit &
