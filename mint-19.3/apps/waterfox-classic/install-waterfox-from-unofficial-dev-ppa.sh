1ff8441b7fb72c1d6d85f2e0c6aa64fe263d64ec
zpangwin <zpangwin@users.noreply.github.com>
2020-02-25 19:23:03 -0500

    waterfox ppa script - updated to take args for controlling symlink handling; run with --help for more info

--- mint-19.3/apps/waterfox-classic/install-waterfox-from-unofficial-dev-ppa.sh
+++ mint-19.3/apps/waterfox-classic/install-waterfox-from-unofficial-dev-ppa.sh
@@ -7,67 +7,125 @@ else
 	exit;
 fi
 
-has_waterfox_kpe_insta;
+noClobberSymlinks="false";
+keepOptInstallSymlinks="false";
+displayHelp="false";
+omitSymlinks="false";
+recreateSymlinks="false";
 
-# See https://www.blackrosetech.com/gessel/2019/06/19/update-waterfox-with-the-new-ppa-on-mint-19-1
-#		https://forums.linuxmint.com/viewtopic.php?t=296666
-#
+for passedarg in "$@"; do
+    #echo "passedarg is $passedarg"
+    if [[ "-h" == "${passedarg}" || "--help" == "${passedarg}" ]]; then
+        displayHelp="true";
+    elif [[ "-n" == "${passedarg}" || "--no-clobber" == "${passedarg}" ]]; then
+        noClobberSymlinks="true";
+    elif [[ "-k" == "${passedarg}" || "--keep-opt-symlinks" == "${passedarg}" ]]; then
+        keepOptInstallSymlinks="true";
+    elif [[ "-o" == "${passedarg}" || "--omit-symlinks" == "${passedarg}" ]]; then
+        omitSymlinks="true";
+    elif [[ "-r" == "${passedarg}" || "--recreate-symlinks" == "${passedarg}" ]]; then
+        recreateSymlinks="true"
+    fi
+done
 
-# add unofficial hawkeye116477 source for waterfox
-addCustomSource waterfox-hawkeye116477-unofficial 'deb http://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/xUbuntu_18.04/ /';
+if [[ "true" == "${displayHelp}" ]]; then
+    echo "OPTIONS:";
+    echo "  -h, --help                 Displays this help text.";
+    echo "";
+    echo "  -n, --no-clobber           No symlinks will be overwritten.";
+    echo "";
+    echo "  -k, --keep-opt-symlinks    Symlinks pointing to /opt/waterfox-classic will not be overwritten.";
+    echo "";
+    echo "  -o, --omit                 Script will not add any new symlinks/overwrite any existing symlinks.";
+    echo "                             This option is only considered during initial install.";
+    echo "";
+    echo "  -r, --recreate-symlinks    Script will recreate symlinks even if application is already installed.";
+    echo "                             Other options such as -n and -k will still apply.";
+    echo "                             This option is not considered during initial install.";
+    echo "";
+    exit;
+fi
 
-# add key for hawkeye116477 repo
-wget -qO - https://download.opensuse.org/repositories/home:hawkeye116477:waterfox/xUbuntu_18.04/Release.key | sudo apt-key add -;
+isWaterfoxKpeInstalled=$(apt search waterfox-classic|grep -P '^i\w*\s+waterfox-classic-kpe'|wc -l);
+if [[ "0" != "${isWaterfoxKpeInstalled}" ]]; then
+    if [[ "true" == "${recreateSymlinks}" && "false" == "${omitSymlinks}" ]]; then
+        echo "Recreating symlinks for Waterfox Classic KPE ...";
+        echo "";
+    else
+        echo "Waterfox Classic KPE is already installed.";
+        echo "";
+        echo "To remove, run:";
+        echo "  sudo apt remove waterfox-classic-kpe waterfox-locale-en";
+        echo "";
+        echo "To recreate symlinks, use -r or --recreate-symlinks";
+        echo "For more details, see help with -h or --help";
+        echo "";
+        exit;
+    fi
+else
+    # See https://www.blackrosetech.com/gessel/2019/06/19/update-waterfox-with-the-new-ppa-on-mint-19-1
+    #		https://forums.linuxmint.com/viewtopic.php?t=296666
+    #
 
-# update local apt cache
-sudo apt update 2>/dev/null >/dev/null;
+    # add unofficial hawkeye116477 source for waterfox
+    addCustomSource waterfox-hawkeye116477-unofficial 'deb http://download.opensuse.org/repositories/home:/hawkeye116477:/waterfox/xUbuntu_18.04/ /';
 
-# waterfox-classic => resolves to waterfox-classic-kpe
-# normally this package prompts you with a package configuration
-# screen that you have to type ENTER on.
-#
-# To avoid the prompt breaking automation, we are going to follow:
-#	http://www.microhowto.info/howto/perform_an_unattended_installation_of_a_debian_package.html
-#
+    # add key for hawkeye116477 repo
+    wget -qO - https://download.opensuse.org/repositories/home:hawkeye116477:waterfox/xUbuntu_18.04/Release.key | sudo apt-key add -;
 
-# install waterfox
-export DEBIAN_FRONTEND=noninteractive
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y waterfox-locale-en waterfox-classic;
+    # update local apt cache
+    sudo apt update 2>/dev/null >/dev/null;
+
+    # waterfox-classic => resolves to waterfox-classic-kpe
+    # normally this package prompts you with a package configuration
+    # screen that you have to type ENTER on.
+    #
+    # To avoid the prompt breaking automation, we are going to follow:
+    #	http://www.microhowto.info/howto/perform_an_unattended_installation_of_a_debian_package.html
+    #
+
+    # install waterfox
+    export DEBIAN_FRONTEND=noninteractive
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y waterfox-locale-en waterfox-classic;
+
+    # update install flag
+    isWaterfoxKpeInstalled=$(which waterfox-classic|wc -l);
+fi
 
 # setup additional symlink
-isWaterfoxInstalled=$(which waterfox-classic|wc -l);
-if [[ "1" == "${isWaterfoxInstalled}" ]]; then
-    waterfoxClassicPath=$(realpath waterfox-classic);
+if [[ "true" != "${omitSymlinks}" && "1" == "${isWaterfoxKpeInstalled}" ]]; then
+    applicationBinPath=$(realpath waterfox-classic);
 
-    # make sure we're not making any recursive symlinks
-    if [[ "/usr/bin/waterfox" != "${waterfoxClassicPath}" ]]; then
-        createOrReplaceLink="true";
-        if [[ -L "/usr/bin/waterfox" ]]; then
-            existingSymlinkPath=$(realpath "/usr/bin/waterfox");
-            if [[ "${existingSymlinkPath}" == "${waterfoxClassicPath}" ]]; then
-                createOrReplaceLink="false";
+    symlinkList="waterfox-kde waterfox-kpe waterfox-classic-kde waterfox-classic-kpe waterfoxk wf wfc";
+    for symlinkName in $(echo "${symlinkList}"); do
+        # if symlink does not exist, simply create it and go to next iteration
+        if [[ ! -L "/usr/bin/${symlinkName}" ]]; then
+            sudo ln -s "${applicationBinPath}" "/usr/bin/${symlinkName}";
+            continue;
+        fi
 
-            # safety in case someone *does* actually want both the website and the hawkeye ppa versions simultaneously...
-            elif [[ -f "${existingSymlinkPath}" && -r "${existingSymlinkPath}" && -x "${existingSymlinkPath}" ]]; then
-                createOrReplaceLink="false";
+        # otherwise, if the -n/--no-clobber flag is set, then also
+        # skip to the next iteration without making any changes
+        if [[ "true" == "${noClobberSymlinks}" ]]; then
+            continue;
+        fi
+
+        # get the symlink target
+        symlinkDest=$(realpath "/usr/bin/${symlinkName}");
+
+        # if the -k/--keep-opt-symlinks flag is set
+        # and the target is under either /opt/waterfox-classic/* or
+        # /opt/waterfox/* then skip to next iteration without changes
+        if [[ "true" == "${keepOptInstallSymlinks}" ]]; then
+            if [[ "/opt/waterfox-classic/" == "${symlinkDest:0:22}" || "/opt/waterfox/" == "${symlinkDest:0:14}" ]]; then
+                continue;
             fi
         fi
 
-        # if true, then create/replace the /usr/bin/waterfox link and repoint it to /usr/bin/waterfox-classic
-        if [[ "true" == "${createOrReplaceLink}" ]]; then
-            # this is the only link we should use the -f flag with
-            # and we are only triggering there isn't an old install
-    		sudo ln -sf "${waterfoxClassicPath}" /usr/bin/waterfox;
+        # finally, check that the existing symlink does not
+        # already point to the correct location
+        if [[ "${symlinkDest}" != "${applicationBinPath}" ]]; then
+            sudo ln -sf "${applicationBinPath}" "/usr/bin/${symlinkName}";
         fi
-	fi
-
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/waterfox-kde;
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/waterfox-kpe;
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/waterfox-classic-kde;
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/waterfox-classic-kpe;
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/waterfoxk;
-
-    # if wf & wfc aren't in use by something else then reserve these too
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/wf;
-    sudo ln -s "${waterfoxClassicPath}" /usr/bin/wfc;
+    done
 fi
