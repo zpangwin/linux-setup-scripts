7d2cbfcb6aa2467c85c142a79484043e854e3504
zpangwin <zpangwin@users.noreply.github.com>
2020-04-16 14:04:06 -0400

    updated chrome script to allow bypass warning with -n or --no-prompt

--- mint-19.3/apps/google-chrome/install-google-chrome.sh
+++ mint-19.3/apps/google-chrome/install-google-chrome.sh
@@ -11,15 +11,28 @@ if [[ ! -f "${SCRIPT_DIR_PARENT}/functions.sh" ]]; then
 fi
 . "${SCRIPT_DIR_PARENT}/functions.sh";
 
-echo "";
-echo "WARNING: Google adds proprietary code to Chrome that may contain privacy-violating \"features\" such as usage behavior tracking and other telemetry data."
-echo "Google Chrome is based on the open-source project Chromium and the two are nearly identical except that Google adds non-open code on top."
-echo "";
-echo "While on Windows Chromium doesn't provide an auto-update service, on Linux you don't have to worry as you will get updates automatically through your package manager.";
-echo "";
-echo "If you value your privacy, it is highly recommended to install and use Chromium instead.";
-echo "";
-read -rsp $'Press enter to continue installing chrome or Ctrl+C to abort ...\n';
+skipPrompt="false";
+if [[ "-n" == "$1" || "--no-prompt" == "$1" ]]; then
+	skipPrompt="true";
+fi
+
+# check for chroot environment
+isChroot="false";
+if [[ "root" == "${USER}" && "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]]; then
+	skipPrompt="true";
+fi
+
+if [[ "true" == "${skipPrompt}" ]]; then
+	echo "";
+	echo "WARNING: Google adds proprietary code to Chrome that may contain privacy-violating \"features\" such as usage behavior tracking and other telemetry data."
+	echo "Google Chrome is based on the open-source project Chromium and the two are nearly identical except that Google adds non-open code on top."
+	echo "";
+	echo "While on Windows Chromium doesn't provide an auto-update service, on Linux you don't have to worry as you will get updates automatically through your package manager.";
+	echo "";
+	echo "If you value your privacy, it is highly recommended to install and use Chromium instead.";
+	echo "";
+	read -rsp $'Press enter to continue installing chrome or Ctrl+C to abort ...\n';
+fi
 
 # add key
 wget -qO - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
