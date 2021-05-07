1812ff08ddd1daeaaf3298bc9db6810e1cbea90e
zpangwin <zpangwin@users.noreply.github.com>
2021-05-07 00:26:34 -0400

    restructured to get rid of 'apps' subfolders

--- mint-20/apps/git-extensions/usr/share/nemo/actions/scripts/launch-gitext-init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/mono "/opt/GitExtensions/GitExtensions.exe" init "$@" &
--- mint-20/apps/git-extensions/usr/share/nemo/actions/scripts/launch-gitext-revert-file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gitTopLevelDir=$("/usr/bin/which-git-top-dir" "$1");
-if [[ "" != "${gitTopLevelDir}" ]]; then
-	cd "${gitTopLevelDir}";
-    /usr/bin/mono "/opt/GitExtensions/GitExtensions.exe" revert "$1" &
-fi
